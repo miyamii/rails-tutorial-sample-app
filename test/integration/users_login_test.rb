24f1cc240c19ecb3edcb8c67b6fff6de75c21300
Misaki Miyata <miyata@rightcode.co.jp>
2018-05-16 18:44:36 +0900

    11章終了

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -52,7 +52,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "login with remembering" do
     log_in_as(@user, remember_me: '1')
-    assert_equal cookies['remember_token'], assigns(:user).remember_token
+    assert_not_empty cookies['remember_token']
   end
 
   test "login without remembering" do
@@ -63,4 +63,5 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     log_in_as(@user, remember_me: '0')
     assert_empty cookies['remember_token']
   end
+
 end
