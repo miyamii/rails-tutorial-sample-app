9619a2d8fdd36a2b07caf974e1525d92b2c07dc8
Misaki Miyata <miyata@rightcode.co.jp>
2018-05-21 15:58:26 +0900

    14章終了 0521

--- app/models/user.rb
+++ app/models/user.rb
@@ -71,9 +71,10 @@ has_many :passive_relationships, class_name:  "Relationship",
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
-
+  # ユーザーのステータスフィードを返す
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
   end
   # ユーザーをフォローする
   def follow(other_user)
